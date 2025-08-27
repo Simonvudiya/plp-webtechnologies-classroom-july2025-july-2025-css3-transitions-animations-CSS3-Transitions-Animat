8a091270e09d5942841ba4345166745933cdb007
simonvudiya <simonvudiya@gmail.com>
2025-08-27 19:34:25 +0300

    Week_4_Assgn

---
+++ Desktop/sql_week_4_assignment/answers.sql
@@ -0,0 +1,25 @@
+# Question 1
+SELECT paymentDate, 
+       SUM(amount) AS totalAmount
+FROM payments
+GROUP BY paymentDate
+ORDER BY paymentDate DESC
+LIMIT 5;
+
+#Quetion 2
+SELECT customerName, 
+       country, 
+       AVG(creditLimit) AS averageCreditLimit
+FROM customers
+GROUP BY customerName, country;
+#Question 3
+SELECT productCode, 
+       quantityOrdered, 
+       SUM(quantityOrdered * priceEach) AS totalPrice
+FROM orderdetails
+GROUP BY productCode, quantityOrdered;
+#Question 4
+SELECT checkNumber, 
+       MAX(amount) AS highestPayment
+FROM payments
+GROUP BY checkNumber;
